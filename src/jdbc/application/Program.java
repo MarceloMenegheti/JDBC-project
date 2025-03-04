6a5ee7c0f18fc5314da275a64efbe343232aff8f
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2025-03-04 10:07:17 -0300

    Fazendo a consulta no DB

---
+++ src/jdbc/application/Program.java
@@ -0,0 +1,44 @@
+package jdbc.application;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import jdbc.Db;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		//conectar o banco
+		Connection conn = null;
+		
+		//fazer uma consulta SQL
+		Statement st = null;
+		
+		//Guardar o resultado desta query
+		ResultSet rs = null;
+		
+		try {
+			//conectando com o DB
+			conn = Db.getConnection();
+			
+			//Instanciando um obj <Statement>
+			st = conn.createStatement();
+			
+			//fazendo uma consulta que irei guardar no ResultSet
+			rs = st.executeQuery("SELECT * FROM Seller");
+			
+			//percorrendo os dados da consulta
+			while(rs.next()) {
+				System.out.println(rs.getInt("Id") + " | " + rs.getString("Name") + " | "+ rs.getString("Email")+ " | " + rs.getInt("BaseSalary"));
+			}
+						
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
