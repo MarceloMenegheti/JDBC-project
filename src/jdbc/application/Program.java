53cb3a064eff63ce6ae86fb6198da916a64ab332
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2025-03-04 14:26:34 -0300

    Protegendo a Transacao com ( .setAutoCommit(false), .commit(),  .rollback())

--- src/jdbc/application/Program.java
+++ src/jdbc/application/Program.java
@@ -6,6 +6,7 @@ import java.sql.Statement;
 import java.text.ParseException;
 
 import db.Db;
+import db.DbException;
 
 public class Program {
 
@@ -19,6 +20,10 @@ public class Program {
 			
 			st = conn.createStatement();
 			
+			//Não confirmar as alterações automaticamente
+			conn.setAutoCommit(false);
+			
+			
 			//todo vendedor que pertence ao department 1 e 2 sera atualizado.
 			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE departmentId = 1");
 			
@@ -30,11 +35,20 @@ public class Program {
 			
 			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3500 WHERE departmentId = 2");
 
+			//para confirmar que a transação acabou 
+			conn.commit();
+			
 			System.out.println("Linha1 " + rows1 + "\nLinha2 " + rows2);
 			
 		}
 		catch(SQLException e) {
-			e.printStackTrace();
+			try {
+				//Voltar ao estado inicial do Banco
+				conn.rollback();
+				throw new DbException("Transacao voltou! causado por :" + e.getMessage());
+			} catch (SQLException e1) {
+				throw new DbException("Erro Tentando voltar a transacao - causado por: "+ e1.getMessage());
+			}
 		}
 		finally {
 			Db.closeStatement(st);
