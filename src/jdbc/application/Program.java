dbbc1c2e601c9417e072cfe47f2cc20d356c5cbc
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2025-03-05 15:43:12 -0300

    Implementando a operacao findById()

--- src/jdbc/application/Program.java
+++ src/jdbc/application/Program.java
@@ -1,60 +1,18 @@
 package jdbc.application;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.ParseException;
-
-import db.Db;
-import db.DbException;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args){
 		
-		Connection conn = null;
-		Statement st = null;
-		try {
-			
-			conn = Db.getConnection();
-			
-			st = conn.createStatement();
-			
-			//Não confirmar as alterações automaticamente
-			conn.setAutoCommit(false);
-			
-			
-			//todo vendedor que pertence ao department 1 e 2 sera atualizado.
-			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE departmentId = 1");
-			
-			//fazendo um erro no BD
-			//int x = 2;
-			//if(x<3) {
-			//	throw new SQLException("Fake error");
-			//}
-			
-			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3500 WHERE departmentId = 2");
-
-			//para confirmar que a transação acabou 
-			conn.commit();
-			
-			System.out.println("Linha1 " + rows1 + "\nLinha2 " + rows2);
-			
-		}
-		catch(SQLException e) {
-			try {
-				//Voltar ao estado inicial do Banco
-				conn.rollback();
-				throw new DbException("Transacao voltou! causado por :" + e.getMessage());
-			} catch (SQLException e1) {
-				throw new DbException("Erro Tentando voltar a transacao - causado por: "+ e1.getMessage());
-			}
-		}
-		finally {
-			Db.closeStatement(st);
-			Db.closeConnection();
-		}
-
+		SellerDao sellerDao = DaoFactory.creteSellerDao();
+		Seller seller = sellerDao.findById(19);
+		
+		System.out.println(seller);
+	
 	}
 
 }
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,17 +1,21 @@
 package model.dao;
 
+import db.Db;
 import model.dao.impl.SellerDaoJDBC;
 
 
 
+//Classe auxiliar para instanciar os Dao(s)
 public class DaoFactory {
 	
-/** È uma forma de fazer a injeção de dependencia sem explicitar a Implementação
- * desta modo o Programa principal não conhece a implementação
- * somente a Interface SellerDao.
- * */
+/* 
+ È uma forma de fazer a injeção de dependencia sem explicitar a Implementação desta modo
+ o Programa principal não conhece a implementação somente a Interface SellerDao.
+ */
 	public static SellerDao creteSellerDao() {
-		return new SellerDaoJDBC();
+		
+		//fazendo a connection
+		return new SellerDaoJDBC(Db.getConnection());
 	}
 
 }
