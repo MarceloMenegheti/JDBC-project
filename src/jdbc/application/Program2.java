bb926e3498c1798c48c31690ba7b8669d51fde30
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2025-03-06 19:54:38 -0300

    Program2 para testar as operações do DepartmentDaoJDBC

---
+++ src/jdbc/application/Program2.java
@@ -0,0 +1,33 @@
+package jdbc.application;
+
+import java.util.Scanner;
+
+public class Program2 {
+
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		
+
+		System.out.println("=== TEST 1: seller findByid ====");
+		
+		
+		System.out.println("\n=== TEST 2: seller findByDepartment ====");
+
+		
+		System.out.println("\n=== TEST 3: seller findAll ====");
+
+		
+		System.out.println("\n=== TEST 4: seller Insert ====");
+
+		
+		System.out.println("\n=== TEST 5: seller Update ====");
+
+		
+		System.out.println("\n=== TEST 6: seller Delete ====");
+
+		
+		sc.close();
+	}
+
+}
