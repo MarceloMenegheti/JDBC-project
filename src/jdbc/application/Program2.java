8ce3af25c15c615363fcdaf6913034bbb44c8669
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2025-03-06 22:21:54 -0300

    Implementando a operacao do Department - Delete()

--- src/jdbc/application/Program2.java
+++ src/jdbc/application/Program2.java
@@ -5,9 +5,7 @@ import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
-import model.dao.impl.DepartmentDaoJDBC;
 import model.entities.Department;
-import model.entities.Seller;
 
 public class Program2 {
 
@@ -17,31 +15,32 @@ public class Program2 {
 		
 		DepartmentDao departmentDao = DaoFactory.creteDepartmentDao();
 		
-		System.out.println("=== TEST 1: seller findByid ====");
+		System.out.println("=== TEST 1: department findByid ====");
 		Department dep = departmentDao.findById(2);
 		System.out.println(dep);
 		
-		System.out.println("\n=== TEST 2: seller findAll ====");
+		System.out.println("\n=== TEST 2: departement findAll ====");
 		List<Department> list = departmentDao.findAll();
 		for(Department obj : list) {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n=== TEST 3: seller Insert ====");
-		Department dep2 = new Department(null, "Pagode");
+		System.out.println("\n=== TEST 3: departement Insert ====");
+		Department dep2 = new Department(null, "D1");
 		departmentDao.insert(dep2);
 		System.out.println("Insert Id = " + dep2.getId());
 		
-		System.out.println("\n=== TEST 4: seller Update ====");
-		Department dep3 = departmentDao.findById(13);
-		dep3.setName("party");
+		System.out.println("\n=== TEST 4: departement Update ====");
+		Department dep3 = departmentDao.findById(12);
+		dep3.setName("ROCK");
 		departmentDao.update(dep3);
 		System.out.println("Update feito com sucesso!");
 		
-
-		
-		System.out.println("\n=== TEST 5: seller Delete ====");
-
+		System.out.println("\n=== TEST 5: departement Delete ====");
+		System.out.print("Entre com um id para deletar: ");
+		int deleteId = sc.nextInt();
+		departmentDao.deleteById(deleteId);
+		System.out.println("Delete feito com sucesso! ");
 		
 		sc.close();
 	}
